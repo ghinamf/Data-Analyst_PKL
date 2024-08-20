2c6ad10117efc26d6c9fb5847d2623d468cb8aab
ghinamf <mufidahg17@gmail.com>
2024-08-20 16:07:54 +0700

    updated

--- code/app.py
+++ code/app.py
@@ -71,7 +71,7 @@ def create_connection():
             user=config.MYSQL_USER,
             password=config.MYSQL_PASSWORD,
             database=config.MYSQL_DATABASE,
-             port=config.port
+             
         )
         if connection.is_connected():
             st.write("Koneksi Berhasil")
